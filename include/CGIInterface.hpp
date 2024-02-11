f8f32032d8da6a8af0a99c92330b9ffe6b14addd
Yuran <yuran653@gmail.com>
2024-02-12 02:54:34 +0700

    Feb12_2-54_in_process

--- include/CGIInterface.hpp
+++ include/CGIInterface.hpp
@@ -6,7 +6,7 @@
 /*   By: jgoldste <jgoldste@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 12:40:47 by jgoldste          #+#    #+#             */
-/*   Updated: 2024/02/11 18:47:20 by jgoldste         ###   ########.fr       */
+/*   Updated: 2024/02/12 02:49:50 by jgoldste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,9 @@
 
 class CGIInterface {
 	private:
-		// static char** _env;
-
 		CGIInterface();
 		~CGIInterface();
 
-		// static char**	_initCGIArgv(const std::string& cgi_pass, const std::string& body_temp_path);
-		// static char**	_initEnv()
-		
 		static int	_checkFile(std::pair<int, std::string>& response,
 			const std::string& path, const int& code);
 		static int	_execute(std::pair<int, std::string>& response,
--- src/CGIInterface.cpp
+++ src/CGIInterface.cpp
@@ -6,7 +6,7 @@
 /*   By: jgoldste <jgoldste@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 12:40:44 by jgoldste          #+#    #+#             */
-/*   Updated: 2024/02/11 19:21:56 by jgoldste         ###   ########.fr       */
+/*   Updated: 2024/02/12 02:53:14 by jgoldste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,22 +18,9 @@ CGIInterface::CGIInterface() {
 CGIInterface::~CGIInterface() {
 }
 
-// char** CGIInterface::_initEnv() {
-
-// }
-
-// void CGIInterface::_checkPath(const std::string& path) {
-// 	std::ofstream os(path, std::ios::out | std::ios::trunc);
-// 	if (os.is_open() == false)
-// 		throw CGIInterfaceError("Failed to open: [" + path + "] file");
-// }
-
-// char** CGIInterface::_initCGIArgv(const std::string& cgi_pass, const std::string& body_temp_path) {
-	
-// }
-
 int	CGIInterface::_execute(std::pair<int, std::string>& response,
-			const std::string& cgi_pass, const std::string& body_temp_path, const int& code) {
+	const std::string& cgi_pass, const std::string& body_temp_path,
+	const int& code) {
 	(void)cgi_pass;
 	(void)body_temp_path;
 	response = std::make_pair(code, "OK");
@@ -47,11 +34,15 @@ int	CGIInterface::_execute(std::pair<int, std::string>& response,
 		dup2(fd[1], STDOUT_FILENO);
 		close(fd[1]);
 		close(fd[0]);
-		if (execl(cgi_pass.c_str(), body_temp_path.c_str()) == -1)
+		if (execl(cgi_pass.c_str(), body_temp_path.c_str(), NULL) == -1)
 			return 1;
 	} else {
+		close(fd[1]);
+
+	
+		
 		int status;
-		waitpid(-1, &status, NULL);
+		waitpid(-1, &status, 0);
 		if (WIFEXITED(status) == -1)
 			return 1;
 			response.first = 200;
@@ -59,7 +50,8 @@ int	CGIInterface::_execute(std::pair<int, std::string>& response,
 	return 0;
 }
 
-int CGIInterface::_checkFile(std::pair<int, std::string>& response, const std::string& path, const int& code) {
+int CGIInterface::_checkFile(std::pair<int, std::string>& response,
+	const std::string& path, const int& code) {
 	std::ofstream os(path);
 	if (os.is_open() == false) {
 		response = std::make_pair(code, "");
