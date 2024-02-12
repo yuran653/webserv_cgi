c585091c6fd52571faa2cd0353dc00db6683c454
Iurii Ovsiannikov <jgoldste@z1t5c2.42bangkok.com>
2024-02-12 19:14:40 +0700

    Feb12_19-14_in_process

--- include/CGIInterface.hpp
+++ include/CGIInterface.hpp
@@ -6,13 +6,14 @@
 /*   By: jgoldste <jgoldste@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 12:40:47 by jgoldste          #+#    #+#             */
-/*   Updated: 2024/02/12 02:49:50 by jgoldste         ###   ########.fr       */
+/*   Updated: 2024/02/12 15:56:22 by jgoldste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CGIINTERFACE_HPP
 #define CGIINTERFACE_HPP
 
+#include <cstring>
 #include <fstream>
 #include <iostream>
 #include <string>
@@ -24,10 +25,14 @@ class CGIInterface {
 		CGIInterface();
 		~CGIInterface();
 
-		static int	_checkFile(std::pair<int, std::string>& response,
+		static int		_checkFile(std::pair<int, std::string>& response,
 			const std::string& path, const int& code);
-		static int	_execute(std::pair<int, std::string>& response,
+		static int		_execute(std::pair<int, std::string>& response,
 			const std::string& cgi_pass, const std::string& body_temp_path, const int& code);
+		static char**	_initArgv(const std::string& cgi_pass, const std::string& body_temp_path);
+		static char**	_initEnv(const std::string& cgi_pass, const std::string& body_temp_path);
+		static int		_deleteServiceArgs(char**& argv, char**& envp, const int& status);
+		static void		_deleteCharArray(char**& array);
 
 	public:
 		static void	executeCGI(std::pair<int, std::string>& response,
--- src/CGIInterface.cpp
+++ src/CGIInterface.cpp
@@ -6,7 +6,7 @@
 /*   By: jgoldste <jgoldste@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 12:40:44 by jgoldste          #+#    #+#             */
-/*   Updated: 2024/02/12 02:53:14 by jgoldste         ###   ########.fr       */
+/*   Updated: 2024/02/12 18:58:26 by jgoldste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,36 +18,77 @@ CGIInterface::CGIInterface() {
 CGIInterface::~CGIInterface() {
 }
 
+void CGIInterface::_deleteCharArray(char**& array) {
+	if (array != NULL) {
+		for (size_t i = 0; array[i]; i++)
+			delete[] array[i];
+		delete[] array;
+		array = NULL;
+	}
+}
+
+int CGIInterface::_deleteServiceArgs(char**& argv, char**& envp, const int& status) {
+	_deleteCharArray(argv);
+	_deleteCharArray(envp);
+	return status;
+}
+
+char** CGIInterface::_initEnv(const std::string& cgi_pass, const std::string& body_temp_path) {
+	char** envp = new char*[2];
+	envp[0] = new char[cgi_pass.size() + 1];
+	std::strcpy(envp[0], ("PATH=" + cgi_pass.erase(0,2)).c_str());
+	envp[1] = NULL;
+	(void)body_temp_path;
+	return NULL;
+}
+
+char** CGIInterface::_initArgv(const std::string& cgi_pass, const std::string& body_temp_path) {
+	char** argv = new char*[3];
+	argv[0] = new char[cgi_pass.size() + 1];
+	std::strcpy(argv[0], cgi_pass.c_str());
+	argv[1] = new char[body_temp_path.size() + 1];
+	std::strcpy(argv[1], body_temp_path.c_str());
+	argv[2] = NULL;
+	return argv;
+}
+
 int	CGIInterface::_execute(std::pair<int, std::string>& response,
 	const std::string& cgi_pass, const std::string& body_temp_path,
 	const int& code) {
-	(void)cgi_pass;
-	(void)body_temp_path;
+	char** argv = _initArgv(cgi_pass, body_temp_path);
+	char** envp = _initEnv(cgi_pass, body_temp_path);
+	std::cout << "argv: " << argv << std::endl;
+	std::cout << "argv[0]" << argv[0] << std::endl;
+	std::cout << "argv[1]" << argv[1] << std::endl;
+	std::cout << "argv[2]" << &argv[2] << std::endl;
+	// if (argv == NULL || envp == NULL)
+	// 	return _deleteServiceArgs(argv, envp, EXIT_FAILURE);
+	std::cout << "--- OK ---" << std::endl;
 	response = std::make_pair(code, "OK");
-	int fd[2];
-	if (pipe(fd) == -1)
-		return 1;
-	pid_t pid = fork();
-	if (pid == -1)
-		return 1;
-	if (pid == 0) {
-		dup2(fd[1], STDOUT_FILENO);
-		close(fd[1]);
-		close(fd[0]);
-		if (execl(cgi_pass.c_str(), body_temp_path.c_str(), NULL) == -1)
-			return 1;
-	} else {
-		close(fd[1]);
+	// int fd[2];
+	// if (pipe(fd) == -1)
+	// 	return 1;
+	// pid_t pid = fork();
+	// if (pid == -1)
+	// 	return 1;
+	// if (pid == 0) {
+	// 	dup2(fd[1], STDOUT_FILENO);
+	// 	close(fd[1]);
+	// 	close(fd[0]);
+	// 	if (execve(argv[0], argv, envp) == -1)
+	// 		return _deleteServiceArgs(argv, envp, EXIT_FAILURE);
+	// } else {
+	// 	close(fd[1]);
 
 	
 		
-		int status;
-		waitpid(-1, &status, 0);
-		if (WIFEXITED(status) == -1)
-			return 1;
-			response.first = 200;
-	}
-	return 0;
+	// 	int status;
+	// 	waitpid(-1, &status, 0);
+	// 	if (WIFEXITED(status) == 0)
+	// 		return _deleteServiceArgs(argv, envp, EXIT_FAILURE);
+	// 	response.first = 200;
+	// }
+	return _deleteServiceArgs(argv, envp, EXIT_SUCCESS);
 }
 
 int CGIInterface::_checkFile(std::pair<int, std::string>& response,
